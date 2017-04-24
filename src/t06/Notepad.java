ac63776970ea5f13b3aa638e326b8a8a73d75bc6
Igor <alterg@niuitmo.ru>
2017-04-24 17:16:26 +0300

    New features and refactoring

--- src/t06/Notepad.java
+++ src/t06/Notepad.java
@@ -4,42 +4,40 @@ package t06;
  * Created by alterG on 23.04.2017.
  */
 public class Notepad {
-    int MAX_ENTRYES = 50;
-    int currentEntryes;
-    boolean isOverflow;
-    NotepadEntry entryes[];
-
-    public boolean isOverflow() {
-        return isOverflow;
-    }
+    private static final int MAX_ENTRIES = 50;
+    private int indexOfLastEntry;
+    private NotepadEntry entryes[];
 
     public Notepad() {
-        entryes = new NotepadEntry[MAX_ENTRYES];
+        entryes = new NotepadEntry[MAX_ENTRIES];
+        indexOfLastEntry = -1; //no entries
     }
 
-    public void addEntry(NotepadEntry entry) {
-        if (!isOverflow) {
-            entryes[currentEntryes++] = entry;
-        }
-        if (currentEntryes == MAX_ENTRYES) {
-            isOverflow = true;
-        }
+    public void addEntry(NotepadEntry entry) throws Exception {
+        if (indexOfLastEntry == MAX_ENTRIES - 1) throw new Exception();
+        entryes[++indexOfLastEntry] = entry;
     }
 
-    public void showEntryes() {
-        for (int i = 0; i < currentEntryes; i++) {
+    public void showEntryes() throws Exception {
+        if (indexOfLastEntry == -1) throw new Exception();
+        for (int i = 0; i <= indexOfLastEntry; i++) {
             System.out.println("Entry #"+i+"\n"+entryes[i].getEntry());
         }
     }
 
-    public void deleteEntry (int number) {
-        for (int i = number; i < currentEntryes - 1; i++) {
+    public void deleteEntry (int number) throws Exception {
+        if (number > indexOfLastEntry || number < 0) {
+            throw new Exception();
+        }
+        for (int i = number; i < indexOfLastEntry - 1; i++) {
             entryes[i] = entryes[i+1];
         }
-        currentEntryes--;
+        indexOfLastEntry--;
     }
 
-    public  void replaceEntry (int number, NotepadEntry entry) {
-        entryes[number] = entry;
+    public  void editEntry (int number, NotepadEntry entry) throws Exception{
+        if (indexOfLastEntry == -1 || number < 0 || number > indexOfLastEntry) throw new Exception();
+        entryes[number].setHeader(entry.getHeader());
+        entryes[number].setContent(entry.getContent());
     }
 }
