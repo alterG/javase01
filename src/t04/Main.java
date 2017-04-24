227f6922bfdaeabed3ff4c6409eb89a9da7be95f
Igor <alterg@niuitmo.ru>
2017-04-24 15:51:10 +0300

    rewrite for new condition
    Old condition is sum of a[0]+a[n]..
    New condition is sum of a[0]+a[1]..

--- src/t04/Main.java
+++ src/t04/Main.java
@@ -15,25 +15,25 @@ public class Main {
         for (int i = 0; i < length; i++) {
             a[i]=in.nextDouble();
         }
-        System.out.println("Max sum is " + mirrorSumMax(a));
+        System.out.println("Max sum is " + pairSumMax(a));
 
     }
 
     /**
      *
      * @param array
-     * @return max of mirror sum a(0)+a(n), a(1)+a(n-1)...
+     * @return max of mirror sum a(0)+a(1), a(2)+a(3)...
      * if length isn't even number central sum equals central element
      */
-    private static double mirrorSumMax (double[] array) {
+    private static double pairSumMax(double[] array) {
         double sum;
         double max = - Double.MAX_VALUE;
-        for (int i = 0; i < (array.length+1)/2; i++) {
-            if (i != array.length/2) {
-                sum = array[i]+array[array.length-i-1];
+        for (int i = 0; i < array.length; i+=2) {
+            if ( (i == array.length-1) && (array.length % 2 != 0) ) {
+                sum = array[i];
             }
             else {
-                sum = array[i];
+                sum = array[i]+array[i+1];
             }
             if (max < sum) {
                 max = sum;
