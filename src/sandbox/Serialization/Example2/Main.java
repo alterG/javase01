aa779b7dd5a1aa9957fa291eed32908264679214
Igor <alterg@niuitmo.ru>
2017-05-26 13:29:25 +0300

    learning java tech moments

--- src/sandbox/Serialization/Example2/Main.java
+++ src/sandbox/Serialization/Example2/Main.java
@@ -15,12 +15,12 @@ public class Main {
         Line line2 = new Line(point2, point3, 2);
         System.out.println(line1);
         System.out.println(line2);
-        System.out.println(getPath("out.txt"));
         try (ObjectOutputStream stream1 = new ObjectOutputStream(new FileOutputStream(getPath("out.txt")))) {
             stream1.writeObject(line1);
-            line1.setIndex(3);          //index didn't change cause object has been serializable
-            stream1.reset();          //CARE! stream data doesn't erase
-            stream1.writeObject(line1);
+            line1.setIndex(3);
+            stream1.writeObject(line1);     // index didn't change cause object has been serialized
+            stream1.reset();                // CARE! stream data don't erase
+            stream1.writeObject(line1);     // Now stream1 has: 2 objects line1 (index = 1) and 1 object line1 (index = 3)
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -42,7 +42,9 @@ public class Main {
         }
     }
 
-    //Note: Files are created by program should be placed in subdirectory of current package (instead of resource directory)
+    // Note: It's wrong way
+    // Files are created by program should be placed in subdirectory of current package (instead of resource directory)
+    @Deprecated
     private static String getPath(String path) {
         Class<Main> cl = Main.class;
         String filePath = "." + new String(".resources." + cl.getPackage().getName() + ".").replace('.', '\\') + path;
@@ -54,3 +56,20 @@ public class Main {
         return cl.getResourceAsStream(path);
     }
 }
+
+//        Output:
+//        Deserializable objects:
+//        Line index = 1
+//        Object reference = sandbox.Serialization.Example2.Line@4f3f5b24
+//        From point 1 = Point{x=1.0, y=1.0}
+//        To point 2 = Point{x=2.0, y=2.0}
+
+//        Line index = 1
+//        Object reference = sandbox.Serialization.Example2.Line@4f3f5b24
+//        From point 1 = Point{x=1.0, y=1.0}
+//        To point 2 = Point{x=2.0, y=2.0}
+
+//        Line index = 3
+//        Object reference = sandbox.Serialization.Example2.Line@7291c18f
+//        From point 1 = Point{x=1.0, y=1.0}
+//        To point 2 = Point{x=2.0, y=2.0}
