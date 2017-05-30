d813e8d907955de81fc17c37067f5e79f19fe10e
Igor <alterg@niuitmo.ru>
2017-05-31 01:44:39 +0300

    refactor + add touch() and exit()

--- src/module5/t01/Main.java
+++ src/module5/t01/Main.java
@@ -1,8 +1,6 @@
 package module5.t01;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -13,7 +11,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Условие:
+ * Разработать приложение, позволяющее просматривать файлы и каталоги файловой
+ * системы, а также создавать и удалять текстовые файлы. Для работы с текстовыми файлами
+ * необходимо реализовать функциональность записи (дозаписи) в файл. Требуется определить
+ * исключения для каждого слоя приложения и корректно их обработать.
+ *
+ * Notes:
  * Files and directories are united in File
+ * Firstly, you should to realize class architecture and methods logic
+ *
+ * ToDo: generate documentation
  * Created by alterG on 30.05.2017.
  */
 public class Main {
@@ -34,30 +42,44 @@ public class Main {
             }
             String argument1 = matcher.group(1);
             int argument2Index;
-            String argument2;
             switch (argument1) {
                 case "ls":
                     ls(pwd);
                     break;
-//                case "cd":
-//                    argument2Index = matcher.end(1);
-//                    File fileToCd = getArgument2(inputBuffer, argument2Index);
-//                    if ("..".equals(argument2)) {
-//                        cd((pwd.getParentFile()));
-//                    } else {
-//                        cd(new File(pwd.getAbsolutePath() +"\\"+argument2));
-//                    }
-//                    break;
+                case "cd":
+                    argument2Index = matcher.end(1);
+                    try {
+                        File fileToCd = getArgument2(inputBuffer, argument2Index, true);
+                        cd(fileToCd);
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
                 case "cat":
                     argument2Index = matcher.end(1);
                     try {
-                        File fileToCat = getArgument2(inputBuffer, argument2Index);
+                        File fileToCat = getArgument2(inputBuffer, argument2Index, true);
                         cat(fileToCat);
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    } catch (IOException e) {
+                    } catch (IllegalArgumentException  | IOException e) {
                         System.out.println(e.getMessage());
                     }
+                    break;
+                case "touch":
+                    argument2Index = matcher.end(1);
+                    try {
+                        File fileToCat = getArgument2(inputBuffer, argument2Index, false);
+                        if (touch(fileToCat)) {
+                            System.out.println("File is created.");
+                        } else {
+                            System.out.println("File can't be created.");
+                        }
+                    } catch (IOException | IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "exit":
+                    System.out.println("Thank you for work.");
+                    System.exit(0);
                     break;
                 default:
                     System.out.println(argument1+" (def)");
@@ -67,43 +89,71 @@ public class Main {
     }
 
     /**
-     *
-     * @param string
-     * @param start
-     * @return
+     * Parse 2nd argument
+     * Note: method makes too much work
+     * @param string to parse 2nd argument
+     * @param start position index to parse
+     * @return argument for command
      * @throws IllegalArgumentException if pwd doesn't contain parsed file (in 2nd argument) or
      * 2nd argument has wrong format
      */
-    private static File getArgument2(String string, int start) throws IllegalArgumentException {
-        Matcher matcher = secondArgument.matcher(string);
+    private static File getArgument2(String string, int start, boolean isExistValidation) throws IllegalArgumentException {
+        Matcher matcher = secondArgument.matcher(string.substring(start));
         if (!matcher.find()) {
             throw new IllegalArgumentException("Wrong argument.");
         }
+        if ("..".equals(matcher.group(1))) return pwd.getParentFile();
+
         File file = new File(pwd.getAbsolutePath());
-        Set<File> fileSet = new HashSet<>(Arrays.asList(file.listFiles()));
-        File parsedFile = new File(pwd.getAbsolutePath()+"\\"+matcher.group(1));
-        if (fileSet.contains(parsedFile)) {
-            return parsedFile;
+        if (file.listFiles() == null) {
+            throw new IllegalArgumentException(("Directory is empty."));
         } else {
-            throw new IllegalArgumentException("File not found.");
+            File parsedFile = new File(pwd.getAbsolutePath() + "\\" + matcher.group(1));
+            if (isExistValidation) {
+            Set<File> fileSet = new HashSet<>(Arrays.asList(file.listFiles()));
+                if (fileSet.contains(parsedFile)) {
+                    return parsedFile;
+                } else {
+                    throw new IllegalArgumentException("File not found.");
+                }
+            } else {
+                return parsedFile;
+            }
         }
     }
 
+    private static boolean touch(File file) throws IOException {
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            throw new IOException("Can't create file.");
+        }
+    }
+
+    /**
+     * Print files in directory
+     * @param file target directory
+     */
     private static void ls(File file) {
         for (File x : file.listFiles()) {
             System.out.println(String.format("%s\t%s", x.isFile() ? "-" : "d", x.getName()));
         }
     }
 
-    private static void cd(File file) {
-        if (file.exists() && file.canRead() && file.canExecute()) {
-            pwd = file;
-        } else {
-            System.out.println("ACCESS DENIED.");
+    /**
+     * Change pwd to given directory
+     * @param file target directory
+     */
+    private static void cd(File file) throws IllegalArgumentException {
+        if (!file.canRead() || !file.canExecute()) {
+            throw new IllegalArgumentException("Access denied.");
         }
+            pwd = file;
     }
 
     /**
+     * Print content of file
+     *
      * Here we can know reason of exception, that's why we throw exception
      * with it's reason after catching it
      * @param file to print it's content
